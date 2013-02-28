519bafbd9617821b6e3b59787adbc7f4c883f052
Ryan Burrows <rhburrows@gmail.com>
2013-02-27 20:24:29 -0500

    Extract host stuff to proper starter-kit file

---
+++ dragonbrains.el
@@ -0,0 +1,5 @@
+(setq-default ispell-program-name "aspell")
+
+(setq browse-url-browser-functoin 'browse-url-generic
+      browse-url-generic-program "/usr/bin/conkeror")
+(add-to-list 'exec-path "/home/rhburrows/bin")
