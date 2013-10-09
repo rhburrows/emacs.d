366809baf1fce0fe3b9d00fa9500a55a447faed3
Ryan Burrows <rhburrows@gmail.com>
2013-10-09 13:13:14 -0700

    Add git-amend function
    Add a function to amend the last commit with the existing commit
    message. Stolen from whattheemacsd.com

---
+++ rhburrows/git.el
@@ -0,0 +1,10 @@
+;; C-c C-a to amend without an prompt
+;; From http://whattheemacsd.com/setup-magit.el-05.html
+(defun magit-just-amend ()
+  (interactive)
+  (save-window-excursion
+    (magit-with-refresh
+      (shell-command "git --no-pager commit --amend --reuse-message=HEAD"))))
+
+(eval-after-load "magit"
+  '(define-key magit-status-mode-map (kbd "C-c C-a") 'magit-just-amend))
