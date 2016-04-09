d73ea11848711a582a91c56aad6f9e7d7d302da9
Ryan Burrows <rhburrows@gmail.com>
2016-04-09 02:54:37 -0700

    Update offsets

--- custom.el
+++ custom.el
@@ -5,11 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(column-number-mode t)
+ '(css-indent-offset 2)
  '(custom-safe-themes
    (quote
     ("a7e8dc00fc8043439a738a15e2f593b8e9b2492f" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "b7553781f4a831d5af6545f7a5967eb002c8daeee688c5cbf33bf27936ec18b3" "7acc0466fce1bc967ce1561c8c4fdcbf4358b4ae692577562a3ed747c109f9d7" default)))
  '(display-time-mode t)
  '(edconf-exec-path "/usr/local/bin/editorconfig")
+ '(js2-basic-offset 2)
  '(rspec-use-rake-flag nil)
  '(rspec-use-rake-when-possible nil)
  '(sql-postgres-login-params
