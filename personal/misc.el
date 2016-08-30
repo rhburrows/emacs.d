a659de676eb432ae2ac6e6ad606bc5aa5596f6c2
Ryan Burrows <rhburrows@gmail.com>
2016-08-30 08:15:26 -0700

    Add some personal settings

---
+++ personal/misc.el
@@ -0,0 +1,6 @@
+(setq prelude-guru nil)
+(setq white-space-line-column 100)
+(scroll-bar-mode -1)
+
+(set-face-attribute 'default (selected-frame) :height 145)
+(global-set-key (kbd "C-c ?") 'dash-at-point)
