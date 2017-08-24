95153cf7d7f528ce8844c0012862e1e6cdf32b19
Ryan Burrows <rhburrows@gmail.com>
2017-08-23 17:23:49 -0700

    Set up electric-pairs for javascript

--- languages/javascript.el
+++ languages/javascript.el
@@ -3,6 +3,11 @@
 
 (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
 
+(add-hook 'web-mode-hook 'electric-pair-mode)
+(add-hook 'web-mode-hook
+	  (lambda ()
+	    (setq-local electric-pair-pairs (append electric-pair-pairs '((?' . ?'))))))
+
 ;; Set up flow
 (add-hook 'web-mode-hook 'flow-minor-mode)
 (with-eval-after-load 'company
