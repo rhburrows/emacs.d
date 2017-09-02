559f53696dbf552215c1c2879d2469863d6a8d6b
Ryan Burrows <rhburrows@gmail.com>
2017-09-02 08:53:28 -0700

    Add rhb/react-native-run command
    If the current buffer is under a react-native project allow running
    react-native commands in compile mode with C-c r

--- languages/javascript.el
+++ languages/javascript.el
@@ -9,7 +9,7 @@
 	    (setq-local electric-pair-pairs (append electric-pair-pairs '((?' . ?'))))))
 (add-hook 'web-mode-hook 'flow-minor-mode)
 
-(defun my/use-eslint-from-node-modules ()
+(defun rhb/use-eslint-from-node-modules ()
   (let* ((root (locate-dominating-file
 		(or (buffer-file-name) default-directory)
 		"node_modules"))
@@ -19,7 +19,7 @@
     (when (and eslint (file-executable-p eslint))
       (setq-local flycheck-javascript-eslint-executable eslint))))
 
-(defun my/use-flow-from-node-modules ()
+(defun rhb/use-flow-from-node-modules ()
   (let* ((root (locate-dominating-file
 		(or (buffer-file-name) default-directory)
 		"node_modules"))
@@ -29,8 +29,8 @@
     (when (and flow (file-executable-p flow))
       (setq-local flycheck-javascript-flow-executable flow))))
 
-(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
-(add-hook 'flycheck-mode-hook #'my/use-flow-from-node-modules)
+(add-hook 'flycheck-mode-hook #'rhb/use-eslint-from-node-modules)
+(add-hook 'flycheck-mode-hook #'rhb/use-flow-from-node-modules)
 
 (require 'flycheck-flow)
 (customize-set-variable 'flycheck-javascript-flow-args '("--respect-pragma"))
@@ -40,3 +40,19 @@
 
 (with-eval-after-load 'company
   (add-to-list 'company-backends 'company-flow))
+
+;; react-native stuff
+(defun rhb/react-native-run (cmd)
+  (interactive "scommand: ")
+  (let* ((root (locate-dominating-file
+		(or (buffer-file-name) default-directory)
+		"node_modules"))
+	 (react-native (and root
+			    (expand-file-name "node_modules/.bin/react-native"
+					      root))))
+    (if (and react-native (file-executable-p react-native))
+	(let ((default-directory root))
+	  (compile (concat react-native " " cmd)))
+      (message "This doesn't look like a react-native project"))))
+
+(define-key web-mode-map (kbd "C-c r") 'rhb/react-native-run)
