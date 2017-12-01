62a2b74fc1656c8892cd531370be3567e4ee276a
Ryan Burrows <ryan.burrows.ctr@doubledown.com>
2017-12-01 13:09:51 -0800

    Add an easier way to add inactive time stamps in org-mode

--- config/org.el
+++ config/org.el
@@ -2,5 +2,7 @@
 
 (setq org-agenda-files '("~/Dropbox/org"))
 
+(define-key org-mode-map (kbd "C-c z") 'org-time-stamp-inactive)
+
 ;; After 10 seconds idle alert due tasks
 (run-with-idle-timer 10 nil 'org-alert-check)
