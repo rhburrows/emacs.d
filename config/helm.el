eb4e2847c02811032e1de160a69c38c7ac73d9a8
Ryan Burrows <rhburrows@gmail.com>
2018-04-06 21:55:17 -0700

    Configure multi-term

--- config/helm.el
+++ config/helm.el
@@ -1,9 +1,8 @@
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-c t") #'helm-mt)
 
 (helm-mode 1)
 
 (helm-projectile-on)
-
-
