45e1754c3a52cee206c538e5f50485fd15c61782
Ryan Burrows <ryan@ip-10-10-2-231.us-west-2.compute.internal>
2018-04-16 15:16:32 -0700

    Start setting up typescript support

---
+++ languages/typescript.el
@@ -0,0 +1,15 @@
+(add-to-list 'auto-mode-alist '("\\.tsx$" . typescript-mode))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(setq company-tooltip-align-annotations t)
+
+; (add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
