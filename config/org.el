bac45a15ecb75b44c041dd35009562e04a8d3d9d
Ryan Burrows <rhburrows@gmail.com>
2018-08-23 10:19:09 -0700

    Update org config

--- config/org.el
+++ config/org.el
@@ -1,5 +1,3 @@
-
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 (setq org-ellipsis "⤵")
 
 (global-set-key (kbd "C-c l") #'org-store-link)
@@ -10,7 +8,11 @@
 (setq org-M-RET-may-split-line nil)
 
 (setq org-agenda-files
-      (list "~/Dropbox/org/tasks.org"))
+      (list
+;       "~/Dropbox/org/inbox.org"
+       "~/Dropbox/org/personal.org"
+       "~/Dropbox/org/projects.org"
+       "~/Dropbox/org/work.org"))
 
 (setq org-capture-templates
       '(("t" "Task" entry (file "~/Dropbox/org/inbox.org")
@@ -20,9 +22,24 @@
         ))
 
 ;; Make sure we can refile tasks from the "Inbox" to projects
-(setq org-refile-targets '(("tasks.org" :level . 2)))
+(setq org-refile-targets '(
+                           ("personal.org" :level . 1)
+                           ("projects.org" :level . 1)
+                           ("work.org" :level . 2)))
 
 ;; org-journal setup
 (setq org-journal-dir "~/Dropbox/org/journal")
 (setq org-journal-file-format "%Y-%m-%d")
 (add-hook 'org-journal-mode-hook (lambda () (auto-fill-mode 1)))
+
+;; Jump to jira task for org-mode subtree
+(defun rhb-jump-to-jira-task ()
+  (interactive)
+  (let ((host (org-entry-get-with-inheritance "JIRA_HOST"))
+        (ticket (org-entry-get nil "JIRA_TICKET")))
+    (browse-url (concat host "/browse/" ticket))))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-x j") 'rhb-jump-to-jira-task)
+            (org-bullets-mode 1)))
