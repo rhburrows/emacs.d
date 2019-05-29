efc14e3b75ec40ce459c30450dd54c85847b13c6
Ryan Burrows <rhburrows@gmail.com>
2019-05-29 08:23:49 -0700

    Update projectile and re-add keybinding

--- config/projectile.el
+++ config/projectile.el
@@ -2,3 +2,8 @@
                                   :test "yarn test"
                                   :run "react-native run-ios")
 (setq projectile-switch-project-action #'projectile-vc)
+
+(add-to-list 'projectile-globally-ignored-directories "fpkg")
+
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
