4ab3f549727874ca2eacdbc881182dbee8be6ea3
Ryan Burrows <rhburrows@gmail.com>
2020-01-11 13:31:34 -0800

    Ignore node_modules from projects

--- config/projectile.el
+++ config/projectile.el
@@ -4,6 +4,7 @@
 (setq projectile-switch-project-action #'projectile-vc)
 
 (add-to-list 'projectile-globally-ignored-directories "fpkg")
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
 
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
