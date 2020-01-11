3495c50642b2add92495d7747def45a75626cd46
Ryan Burrows <rhburrows@gmail.com>
2020-01-11 13:31:26 -0800

    Update magit diff style

--- config/misc.el
+++ config/misc.el
@@ -36,6 +36,7 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (global-magit-file-mode 1)
+(setq magit-diff-refine-hunk 'all)
 
 (require 'ansi-color)
 (defun colorize-compilation-buffer ()
