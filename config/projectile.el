9cf797286029c2e0e0753b17c4a0d04d3b304383
Ryan Burrows <rhburrows@gmail.com>
2020-05-16 09:41:51 -0700

    Open magit when switching to a project

--- config/projectile.el
+++ config/projectile.el
@@ -3,7 +3,6 @@
   :bind
   ("C-c p" . projectile-command-map)
   :config
-  (setq projectile-switch-project-action #'projectile-vc)
   (add-to-list 'projectile-globally-ignored-directories "fpkg")
   (add-to-list 'projectile-globally-ignored-directories "node_modules")
   (projectile-mode +1)
@@ -11,10 +10,10 @@
   (use-package counsel-projectile
     :ensure t
     :config
+    (setq counsel-projectile-switch-project-action 'counsel-projectile-switch-project-action-vc)
     (counsel-projectile-mode))
 
   (use-package treemacs-projectile
     :ensure t
     :after treemacs
     ))
-  
