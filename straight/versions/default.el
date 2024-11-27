2ceac60d8e820bd1ae18ea021dcbaa8681e3648d
Ryan Burrows <rhburrows@gmail.com>
2024-11-27 14:23:59 -0800

    Set up straight.el for package management

---
+++ straight/versions/default.el
@@ -0,0 +1,10 @@
+(("bind-key" . "ae3197044f47f6a83897dd70762cb58c6efcffa1")
+ ("el-get" . "847901f07bdf67763fa3a6c0fb057048cd58603b")
+ ("emacsmirror-mirror" . "2aab409d5fa93cf18cbabd86da9e4b310c454d51")
+ ("exec-path-from-shell" . "4896a797252fbfdac32fb77508500ac7d220f717")
+ ("gnu-elpa-mirror" . "97f6fd99aa7184c6bcd23cffc7653b8212479cc6")
+ ("melpa" . "e79a7d06e7c933c16b921e0065e210aa958c345d")
+ ("nongnu-elpa" . "4b8b3633c7e29412c89f1c9d6ad7a951772be758")
+ ("straight.el" . "33fb4695066781c634ff1c3c81ba96e880deccf7")
+ ("use-package" . "fc8449bb593de8fce0a8bdd4ee2cb78219cf8c76"))
+:gamma
