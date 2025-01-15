d8d79090cabc3707b6e50b7702e1e7afb7ffad43
Ryan Burrows <ryanb@remitly.com>
2025-01-15 11:30:56 -0800

    Add support for neon

---
+++ config/neon.el
@@ -0,0 +1,5 @@
+(use-package neon-mode
+  :straight (neon-mode
+             :type git
+             :host github
+             :repo "Fuco1/neon-mode"))
--- init.el
+++ init.el
@@ -61,6 +61,7 @@
 
 ;; Language specific configurations
 (load-config-file "markdown.el")
+(load-config-file "neon.el")
 (load-config-file "php.el")
 (load-config-file "rust.el")
 (load-config-file "typescript.el")
