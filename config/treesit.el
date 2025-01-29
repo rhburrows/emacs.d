8a25ad3dfd6c5740b86fbc25e5de49e16da434bb
Ryan Burrows <ryanb@remitly.com>
2025-01-29 08:00:39 -0800

    Add json treesitter source

--- config/treesit.el
+++ config/treesit.el
@@ -1,7 +1,8 @@
 (use-package emacs
   :init
   (setq treesit-language-source-alist
-        '((rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
+        '((json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.24.8"))
+          (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
           (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
           (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
           (yaml . ("https://github.com/tree-sitter-grammars/tree-sitter-yaml" "v0.7.0"))))
