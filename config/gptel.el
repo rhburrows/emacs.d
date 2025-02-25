0b3b6174b54ad4989f8a3c5529195d8684242668
Ryan Burrows <rhburrows@gmail.com>
2025-02-24 21:26:39 -0800

    Fix typo

--- config/gptel.el
+++ config/gptel.el
@@ -7,4 +7,4 @@
    gptel-backend (gptel-make-ollama "Ollama"
                    :host "localhost:11434"
                    :stream t
-                   :models '("qwen2.5-coder:14b")))
+                   :models '("qwen2.5-coder:14b"))))
