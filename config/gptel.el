a4493a14b5b5d39f87c19417c0479c0d972bfc6d
Ryan Burrows <rhburrows@gmail.com>
2025-02-24 21:06:33 -0800

    Update default model settings

--- config/gptel.el
+++ config/gptel.el
@@ -3,10 +3,8 @@
   :config
   (setq
    gptel-default-mode 'org-mode
-   gptel-model "mistral"
+   gptel-model "qwen2.5-coder:14b"
    gptel-backend (gptel-make-ollama "Ollama"
                    :host "localhost:11434"
                    :stream t
-                   :models '("mistral" "phi4"))))
-
-
+                   :models '("qwen2.5-coder:14b")))
