f272c46be23a78679eaab2cf18f37374f459a4db
Ryan Burrows <rhburrows@gmail.com>
2025-05-12 08:16:48 -0700

    Set up launch dashboard

--- config/emacs.el
+++ config/emacs.el
@@ -72,3 +72,26 @@
    ("C-h k" . helpful-key)
    ("C-h x" . helpful-command)
    ("C-c C-d" . helpful-at-point)))
+
+(use-package dashboard
+  :after nerd-icons
+
+  :config
+  (dashboard-setup-startup-hook)
+
+  :custom
+  (dashboard-startup-banner 'logo)
+  (dashboard-banner-logo-title nil)
+  (dashboard-center-content t)
+  (dashboard-set-footer nil)
+
+  (dashboard-display-icons-p t)
+  (dashboard-icon-type 'nerd-icons)
+  (dashboard-set-heading-icons t)
+  (dashboard-set-file-icons t)
+
+  (dashboard-items '((projects . 5)
+                     (bookmarks . 5)
+                     (recents . 5)
+                     (registers . 5))))
+
