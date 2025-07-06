d57137b5fa30a5f0cfb8b007b4a7bdf009a4a069
Ryan Burrows <rhburrows@gmail.com>
2025-07-05 19:06:45 -0700

    Tweak some settings to try and make things a little more performant

--- config/emacs.el
+++ config/emacs.el
@@ -1,52 +1,9 @@
 (use-package emacs
-  :custom
-  (inhibit-splash-screen t)
-  (initial-scratch-message nil)
-  (visible-bell t)
-  (column-number-mode t)
-  (whitespace-line-column 80)
-  (whitespace-style '(face tabs empty trailing lines-tail))
-  (locale-coding-system 'utf-8)
-  (coding-system-for-read 'utf-8)
-  (default-process-coding-system '(utf-8-unix . utf-8-unix))
-  (global-auto-revert-non-file-buffers t)
-  (make-backup-files nil)
-  (auto-save-default nil)
-  (tab-width 2)
-  (indent-tabs-mode nil)
-  (xref-search-program 'ripgrep)
-  (frame-resize-pixelwise t)
-  (ns-pop-up-frames nil)
-  (winner-mode 1)
-  (editorconfig-mode 1)
-
-  (enable-recursive-minibuffers t)
-  (read-extended-command-predicate #'command-completion-default-include-p)
-
-  :hook
-  (text-mode . flyspell-mode)
-
   :config
-  (require 'whitespace)
-  (defalias 'yes-or-no-p 'y-or-n-p)
-  (windmove-default-keybindings)
-
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (display-time-mode t)
-  (global-display-line-numbers-mode 1)
-  (electric-pair-mode t)
-  (show-paren-mode t)
-  (global-auto-revert-mode t)
-  (repeat-mode)
-
   (defvar-keymap scroll-keymap
     :repeat t
     "u" #'scroll-down-command
     "v" #'scroll-up-command)
-
-  (set-charset-priority 'unicode)
 
   ;; Originally from
   ;; https://emacsredux.com/blog/2025/06/01/let-s-make-keyboard-quit-smarter/
@@ -65,7 +22,32 @@ are defining or executing a macro."
           (abort-recursive-edit))
       (unless (or defining-kbd-macro
                   executing-kbd-macro)
-        (funcall-interactively quit)))))
+        (funcall-interactively quit))))
+  
+  :custom
+  (jit-lock-defer-time 0.05)
+  (read-process-output-max (* 1024 1024))
+
+  (inhibit-splash-screen t)
+  (initial-scratch-message nil)
+  (visible-bell t)
+
+  (windmove-default-keybindings)
+
+  (menu-bar-mode nil)
+  (tool-bar-mode nil)
+  (scroll-bar-mode nil)
+  (display-time-mode t)
+  (global-display-line-numbers-mode t)
+  (electric-pair-mode t)
+  (show-paren-mode t)
+  (global-auto-revert-mode t)
+  (repeat-mode t)
+  (set-charset-priority 'unicode)
+  (redisplay-skip-fontification-on-input t)
+  (inhibit-compacting-font-caches t)
+
+  (bidi-display-reordering 'left-to-right))
 
 (use-package which-key
   :demand
--- config/terminal.el
+++ config/terminal.el
@@ -1,4 +1,9 @@
 (use-package vterm
+  :custom
+  ;; Not strictly terminal related but...
+  (read-process-output-max (* 2 1024 1024))
+  (process-adaptive-read-buffering nil)
+
   :hook
   ((vterm-mode . (lambda () (display-line-numbers-mode 0)))))
 
