ab254eda3a5ac00a054141ec0879c490f100f7e5
Ryan Burrows <rhburrows@gmail.com>
2025-10-31 16:57:22 -0700

    Hook mise-mode into eshell

--- config/prog.el
+++ config/prog.el
@@ -81,8 +81,9 @@
   :after (consult-eglot embark))
 
 (use-package mise
-  :hook
-  (prog-mode . mise-mode)
+  :hook (
+         (prog-mode . mise-mode)
+         (eshell-mode . mise-mode))
 
   :custom
-  (mise-update-on-eshell-directory-change))
+  (mise-update-on-eshell-directory-change t))
