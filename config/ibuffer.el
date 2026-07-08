af1ee2cdb9d6fdc156df38144163e74a1ab33700
Ryan Burrows <ryanb@remitly.com>
2026-07-08 05:25:35 -0700

    More ibuffer setings

--- config/ibuffer.el
+++ config/ibuffer.el
@@ -2,7 +2,6 @@
 
 (use-package ibuffer
   :config
-  (global-set-key (kbd "C-x C-b") 'ibuffer)
   (setq ibuffer-saved-filter-groups
         '(("default"
            ("Claude Code" (name . "^\\*claude-code"))
@@ -11,29 +10,60 @@
                            (derived-mode . prog-mode)
                            (not (name . "^\\*scratch\\*$"))))
 
-           ("Git" (name . "^\\magit"))
+           ("Git" (name . "^\\magit:"))
            ("Terminal" (or
                         (mode . eshell-mode)
                         (mode . ghostel-mode)))
+           ("Org" (and
+                   (not (name . "^\\.remarks.org$"))
+                   (mode . org-mode)))
            ("Dired" (mode . dired-mode))
            ("Emacs" (or
                      (name . "^\\*scratch\\*$")
                      (name . "^\\*Messages\\*$")
                      (name . "^\\*Async-native-compile-log\\*$")
                      (name . "^\\*straight-process\\*$")
+                     (name . "^\\*Backtrace\\*$")
                      (name . "^\\*dashboard\\*$")))
            ("Help" (or
                     (mode . help-mode)
                     (mode . Info-mode)
                     (mode . helpful-mode))))))
 
+  (define-ibuffer-column icon
+    (:name "" :inline t)
+    (cond
+     ((buffer-file-name)
+      (nerd-icons-icon-for-buffer))
+     (major-mode (nerd-icons-icon-for-mode major-mode))
+     (t "")))
+
+  :bind
+  (:map global-map ("C-x C-b" . ibuffer))
+
   :hook
+  (ibuffer-mode . hl-line-mode)
   (ibuffer-mode . (lambda ()
-                    (local-unset-key (kbd "M-o")) ; We want ace-window to be truly global
+                    (display-line-numbers-mode -1)
+                    (local-unset-key (kbd "M-o"))
                     (ibuffer-switch-to-saved-filter-groups "default")))
 
   :custom
   (ibuffer-expert t)
+  (ibuffer-display-summary nil)
   (ibuffer-human-readable-size t)
   (ibuffer-default-sorting-mode 'recency)
-  (ibuffer-show-empty-filter-groups nil))
+  (ibuffer-default-shrink-to-minimum-size nil)
+  (ibuffer-show-empty-filter-groups nil)
+  (ibuffer-formats
+   '((mark modified read-only locked " "
+           (icon 2 2)
+           (name 30 30 :left :elide)
+           " "
+           (size 9 -1 :right)
+           " "
+           (mode 16 16 :left :elide)
+           " " filename-and-process)
+     (mark " "
+           (name 16 -1)
+           " " filename))))
