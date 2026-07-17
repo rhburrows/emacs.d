3eda575d1f817f0f84856f9e55afedb3398bb68c
Ryan Burrows <rhburrows@gmail.com>
2026-07-16 21:05:34 -0700

    Work in progress ai-implement function

--- config/ai.el
+++ config/ai.el
@@ -12,8 +12,56 @@
                    :models '("mistral-small3.2:latest"))))
 
 (use-package agent-shell
-  :commands (agent-shell-anthropic-start-claude-code
-             agent-shell-opencode-start-agent))
+  :config
+  (defcustom rhb/ai-implement-prompt
+    "Implement the body of this function. Use the function signature and name to infer intent. Return only the complete function."
+    "prompt"
+    :type 'string)
+
+  (defun rhb/ai--config ()
+    "Return agent config based on transient state."
+    (interactive)
+    (let ((values (when (bound-and-true-p transient--prefix)
+                    (transient-get-value))))
+      (if (alist-get "--claude" values nil nil #'equal)
+          (agent-shell-anthropic-make-claude-code-config)
+        (agent-shell-opencode-make-agent-config))))
+
+  (defun rhb/ai-start-shell ()
+    "Start a new agent shell using transient options."
+    (interactive)
+    (let* ((config (rhb/ai--config))
+           (shell-buffer (agent-shell--dwim :config config :new-shell t)))
+      (when-let ((prompt (rhb/ai--prompt)))
+        (agent-shell-insert :text prompt :shell-buffer shell-buffer))))
+
+  (defun rhb/ai-implement ()
+    "Implement the body of the surrounding function signature."
+    (interactive)
+    (let* ((config (rhb/ai--config))
+           (context (if (region-active-p)
+                        (agent-shell--get-region-context :deactivate t)
+                      (agent-shell--get-current-line-context)))
+           (text (concat rhb/ai-implement-prompt "\n\n" context))
+           (shell-buffer (agent-shell--start :config config
+                                             :new-session t
+                                             :session-strategy 'new
+                                             :no-focus t)))
+      (agent-shell--display-buffer shell-buffer)
+      (agent-shell-insert :text text :shell-buffer shell-buffer)))
+
+  (transient-define-prefix rhb/ai-transient ()
+    "AI Transient menu"
+    ["Options"
+     ("-c" "Use Claude" "--claude")]
+    ["Actions"
+     ("s" "Start shell" rhb/ai-start-shell)
+     ("i" "Implement function" rhb/ai-implement)]
+    ["Review"
+     ("r" "Agent review" agent-review)])
+
+  :bind
+  ("C-c '" . rhb/ai-transient))
 
 (use-package agent-review
   :commands (agent-review)
@@ -23,10 +71,3 @@
              :repo "nineluj/agent-review"
              :files ("*.el")))
 
-(transient-define-prefix rhb/ai-transient ()
-  "AI Transient menu"
-  ["Commands"
-   ("c" "Claude Code" agent-shell-anthropic-start-claude-code)
-   ("o" "Opencode" agent-shell-opencode-start-agent)
-   ("r" "Agent review" agent-review)])
-(global-set-key (kbd "C-c '") 'rhb/ai-transient)
