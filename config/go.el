d2838781ebd85e8459cd2cb7252a32f31ca600c2
Ryan Burrows <ryanb@remitly.com>
2026-08-05 08:02:06 -0700

    Configure consult-imenu narrowing for Go

--- config/go.el
+++ config/go.el
@@ -2,10 +2,23 @@
 
 
 (use-package go-ts-mode
+  :after consult
+
   :mode (("\\.go\\'" . go-ts-mode)
          ("/go\\.mod\\'" . go-mod-ts-mode))
 
   :hook (go-ts-mode . eglot-ensure)
 
   :custom
-  (go-ts-mode-indent-offset 2))
+  (go-ts-mode-indent-offset 2)
+
+  :config
+  (add-to-list 'consult-imenu-config
+               '(go-ts-mode :toplevel "Function"
+                            :types ((?f "Function" font-lock-function-name-face)
+                                    (?m "Method" font-lock-function-name-face)
+                                    (?s "Struct" font-local-type-face)
+                                    (?i "Interface" font-local-type-face)
+                                    (?t "Type" font-local-type-face)
+                                    (?a "Alias" font-local-type-face)))))
+
